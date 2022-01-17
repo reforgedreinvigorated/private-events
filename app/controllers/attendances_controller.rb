051cf88459aa84581f76aaba105d88abdf2ae9bc
reforgedreinvigorated <briantuquoc.le@gmail.com>
2022-01-17 01:58:22 -0600

    added functionality to display past/upcoming events.

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,24 @@
+class AttendancesController < ApplicationController
+    before_action :is_logged_in?
+
+    def create
+        event = Event.find(params[:event_id])
+        @attendance = current_user.attendances.build(attended_event_id: event.id)
+        if @attendance.save
+            flash[:notice] = "You are attending #{event.name}"
+            redirect_to user_path
+        else
+            flash[:alert] = "You are already attending #{event.name}"
+            redirect_to event_path(params[:event_id])
+        end
+    end
+
+    
+    private
+
+    def is_logged_in?
+        unless user_signed_in?
+            redirect_to new_user_session_path
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events, only: [:new, :create, :index, :show]
   resources :users
+  resources :attendances, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "events#index"
 end
